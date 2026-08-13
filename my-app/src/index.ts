54da3c0103ad42e6de1395a6e8f03af31bdc9cc1
tahiamubin <tahia.mubin1@gmail.com>
2026-08-13 20:14:46 +0600

    Update gitignore and index.ts

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,12 +1,37 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+ 
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running");
